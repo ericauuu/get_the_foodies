bfd683a059f66f1db50ed1e979b28cb39d4d4c7c
ericauuu <myuerica@gmail.com>
2019-10-21 15:17:40 -0700

    upload files

--- app.py
+++ app.py
@@ -4,42 +4,28 @@ import matplotlib
 matplotlib.use('Agg')
 from flask import Flask, render_template, request
 import matplotlib.pyplot as plt
-from python.functions import  get_pos_neg_words_df, f1_ind, full_df, get_f1scr, show_aud
+from python.functions import  result_f1scr, result_table
 
 app = Flask(__name__, static_url_path="/static")
 
 @app.route("/")
 def index():
     """Return the main page."""
-    return render_template("index.html")
+
+    restaurants = ["amelie_nyc", "lovemama_nyc", "upstate_nyc"]
+
+    return render_template("index.html", restaurants=restaurants)
 
 @app.route("/get_results", methods=["POST"])
 def get_results():
     data = request.form
     print(data)
-    # user_number = int(data["number"])
-    # user_number_doubled = user_number * 2
 
-    url = data["yelp_url"] 
-    df = full_df(url)
-    words = show_aud(df)
-    table = words.to_html()
-    f1 = get_f1scr(df)
+    restaurant_a = data["restaurant"]
+    table = result_table(restaurant_a).to_html()
+    f1 = result_f1scr(restaurant_a)
 
-    #show_plot(url)
-    #f1_score = f1_ind(url)
-    #text = force_text_ind(url)
-    #force_plot = force_plot_ind(url)
-
-    #return render_template("results.html", url=url, path='/static/images/shap_plot.png')
-    return render_template('results.html', table=table, f1=f1)
-
-#@app.route('/test')
-# def show_plot(url):
-    #draw=force_plot_ind(url)
-    # plt.plot(draw)
-    #draw.savefig('static/images/shap_plot1.png')
-    # return render_template('results.html', name = 'review example', url ='static/images/shap_plot1.png')
+    return render_template('results.html', restaurant = restaurant_a, table=table, f1=f1)
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=5000)
---
+++ python/functions.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import pickle, random
+
+
+def result_table(name):
+    """present the table for the selected restuarant"""
+    
+    with open('../get_the_foodies_demo/static/result_table/'+name+'.pickle', 'rb') as f:
+        table = pickle.load(f)
+    
+    return table
+
+def result_f1scr(name):
+
+    """present the f1 score for the model"""
+    
+    with open('../get_the_foodies_demo/static/f1_scr/'+name+'.pickle', 'rb') as f:
+        f1scr = pickle.load(f)
+    
+    return f1scr
